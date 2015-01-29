c1cfb36881867b348c35f70f4da1c083bec089b0
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2015-01-29 11:59:40 +0100

    Added Google+ Sign-in button

---
+++ main/Android/CloudTaskDo/src/main/java/com/productivity/cloudtaskdo/SignInActivity.java
@@ -0,0 +1,126 @@
+package com.productivity.cloudtaskdo;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+
+
+/**
+ * A login screen that offers login via Google+ sign in.
+ */
+public class SignInActivity extends PlusBaseActivity {
+
+    // UI references.
+    private View mProgressView;
+    private SignInButton mPlusSignInButton;
+    private View mSignOutButtons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // Find the Google+ sign in button.
+        mPlusSignInButton = (SignInButton) findViewById(R.id.plus_sign_in_button);
+        if (supportsGooglePlayServices()) {
+            // Set a listener to connect the user when the G+ button is clicked.
+            mPlusSignInButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    signIn();
+                }
+            });
+        } else {
+            // Don't offer G+ sign in if the app's version is too low to support Google Play
+            // Services.
+            mPlusSignInButton.setVisibility(View.GONE);
+            return;
+        }
+
+        mProgressView = findViewById(R.id.login_progress);
+        mSignOutButtons = findViewById(R.id.plus_sign_out_buttons);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    @Override
+    protected void onPlusClientSignIn() {
+        //Set up sign out and disconnect buttons.
+        Button signOutButton = (Button) findViewById(R.id.plus_sign_out_button);
+        signOutButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+        Button disconnectButton = (Button) findViewById(R.id.plus_disconnect_button);
+        disconnectButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revokeAccess();
+            }
+        });
+    }
+
+    @Override
+    protected void onPlusClientBlockingUI(boolean show) {
+        showProgress(show);
+    }
+
+    @Override
+    protected void updateConnectButtonState() {
+        boolean connected = getPlusClient().isConnected();
+
+        mSignOutButtons.setVisibility(connected ? View.VISIBLE : View.GONE);
+        mPlusSignInButton.setVisibility(connected ? View.GONE : View.VISIBLE);
+    }
+
+    @Override
+    protected void onPlusClientRevokeAccess() {
+        // TODO: Access to the user's G+ account has been revoked.  Per the developer terms, delete
+        // any stored user data here.
+    }
+
+    @Override
+    protected void onPlusClientSignOut() {
+
+    }
+
+    /**
+     * Check if the device supports Google Play Services.  It's best
+     * practice to check first rather than handling this as an error case.
+     *
+     * @return whether the device supports Google Play Services
+     */
+    private boolean supportsGooglePlayServices() {
+        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) ==
+                ConnectionResult.SUCCESS;
+    }
+
+}
+
+
+
